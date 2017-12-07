4a8a85f0ba4580f6c43d8e01bf8b57c1f9b12aa2
DESKTOP-OB6MC9Q\Sena Nur KİRAZ <sena-kiraz@hotmail.com>
2017-12-08 01:25:08 +0300

    sena1

---
+++ off day off time/off day off time/giris_sayfası.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class giris_sayfası
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(182, 245)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'giris_sayfası
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 341)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "giris_sayfası"
+        Me.Text = "giris_sayfası"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
---
+++ off day off time/off day off time/giris_sayfası.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Data.SqlClient
+Public Class giris_sayfası
+    Private Sub giris_sayfası_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim kayitsayisi As Integer
+        kayitsayisi = -1
+        Dim con As SqlConnection = New SqlConnection("Data Source=DESKTOP-OB6MC9Q;Initial Catalog=off_day_off_time;Integrated Security=True")
+        Dim cmd As SqlCommand = New SqlCommand("select count (*)from kullanici", con)
+        Dim rdr As SqlDataReader
+        con.Open()
+
+        kayitsayisi = Convert.ToInt32(cmd.ExecuteScalar())
+        con.Close()
+        Dim cd As SqlCommand = New SqlCommand("select resim_yolu from kullanici", con)
+
+        Dim j As Integer = 0
+
+        Dim resimler(kayitsayisi) As PictureBox
+        For i = 0 To kayitsayisi
+
+            j = j + 100
+            resimler(i) = New PictureBox()
+
+
+            resimler(i).Location = New Point(j, 100)
+
+            resimler(i).Visible = True
+            resimler(i).BackColor = Color.Blue
+            resimler(i).SizeMode = PictureBoxSizeMode.AutoSize
+
+            Me.Controls.Add(resimler(i))
+
+        Next
+        con.Open()
+        rdr = cd.ExecuteReader()
+
+        'resimler = rdr.
+
+
+        rdr.Close()
+        con.Close()
+        'For Each 
+        '    j = j + 100
+        '    resimler(i) = New PictureBox()
+
+
+        '    resimler(i).Location = New Point(j, 100)
+
+        '    resimler(i).Visible = True
+        '    resimler(i).BackColor = Color.Transparent
+        '    resimler(i).SizeMode = PictureBoxSizeMode.AutoSize
+
+        '    Me.Controls.Add(resimler(i))
+
+
+        'Next
+
+
+
+        'Dim cd As SqlCommand = New SqlCommand("select resim_yolu from kullanici ")
+        'cd.Connection = con
+        'con.Open()
+        'Dim oku As SqlDataReader
+        'oku = cd.ExecuteReader()
+
+        'While (oku.Read())
+
+        '    pb = Convert.toku
+        'End While
+
+        'oku.Close()
+        'con.Close()
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Bilgiler.Show()
+
+    End Sub
+
+End Class
